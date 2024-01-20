3a06b7582611b7a2c2db1d585927dd25286547bf
ITexnik <itexnik2023@gmail.com>
2024-01-20 16:51:09 +0600

    пришелец появляется рандомно и за экраном

--- alien.py
+++ alien.py
@@ -5,14 +5,15 @@ import random
 class Alien:
     def __init__(self,screen,ai_settings):
         super(Alien,self).__init__()
+        self.coordinate_x = random.randint(0,ai_settings.screen_width - 100)
         self.screen = screen
         self.ai_settings = ai_settings
         self.image = pygame.image.load("images/alien.bmp")
         self.image = pygame.transform.scale(self.image,(50,50))
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        self.rect.x = self.screen_rect.left
-        self.rect.y = self.screen_rect.top
+        self.rect.x = self.screen_rect.left + self.coordinate_x
+        self.rect.y = self.screen_rect.top - 200
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
 
--- черновик.py
+++ черновик.py
@@ -5,11 +5,18 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1600,800))
     pygame.display.set_caption("Alien Invasion")
+    image = pygame.image.load("images/alien.bmp")
+    rect = image.get_rect()
+    image = pygame.transform.scale(image,(100,100))
+    screen_rect = screen.get_rect()
+    rect.x = screen_rect.left + 1500
+
     while True:
 
         for event in pygame.event.get():
             screen.fill((0,0,255))
             if event.type == pygame.QUIT:
                 sys.exit()
-    pygame.display.flip()
+        screen.blit(image, rect)
+        pygame.display.flip()
 run_game()
